20ff72ca9f99869e6d8d2117eeb6a037be442c35
olesgedz <gedzoles@gmail.com>
2021-04-27 17:27:04 +0300

    web

---
+++ Assets/Scripts/BundleLoaderAsync.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class BundleLoaderAsync : MonoBehaviour
+{
+    public string bundleName;
+    public string assetName;
+    
+    IEnumerator Start()
+    {
+        AssetBundleCreateRequest asyncBundleRequest =
+            AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, bundleName));
+        AssetBundle localAssetBundle = asyncBundleRequest.assetBundle;
+        if (localAssetBundle == null)
+        {
+            Debug.LogError("Failed to load AssetBundle!");
+            yield break;
+        }
+
+        AssetBundleRequest assetRequest = localAssetBundle.LoadAssetAsync<GameObject>(assetName);
+        yield return assetRequest;
+        
+        GameObject prefab = assetRequest.asset as GameObject;
+        Instantiate(prefab);
+        localAssetBundle.Unload(false);
+    }
+
+ 
+}
---
+++ Assets/Scripts/BundleWebLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BundleWebLoader : MonoBehaviour
+{
+  public string bundleUrl = "http://localhost/assetbundles/testbundle";
+  public string assetName = "BundledSpriteObject";
+
+  private IEnumerator Start()
+  {
+    using (WWW web = new WWW(bundleUrl))
+    {
+      yield return web;
+      AssetBundle remoteAssetBundle = web.assetBundle;
+      if (remoteAssetBundle == null)
+      {
+        Debug.LogError("Failed to download AssetBundle");
+        yield break;
+      }
+
+      Instantiate(remoteAssetBundle.LoadAsset(assetName));
+      remoteAssetBundle.Unload(false);
+    }
+  }
+}
